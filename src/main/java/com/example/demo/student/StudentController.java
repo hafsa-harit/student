2ea32656dc33e19e9d8336f779b2b4e590e93235
hafsa <harithafsa@gmail.com>
2022-11-16 14:31:53 +0100

    Second commit

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -15,15 +15,39 @@ public class StudentController {
 
 
 
-
+    //the fact that this controller is a RestController get us the possibility t not
+    //define the object retunred from this method by the annotation : @RispenseBody
     @GetMapping
     public List<Student> getStudent(){
         return  studentService.getStudent();
     }
+
+
     //it's an implementation of the post method, which will use the json object sent on the request body as
     //a parameter to add it using the addStudent method
     @PostMapping
-    public void registerNewStudent (@RequestBody Student student){
+    public void registerNewStudent (
+            @RequestBody Student student){
         studentService.addNewStudent(student);
     }
+    //here we will take the parameter studentID from the uri and send it to the studentService so we can
+    //use it to find the student we want to delete using studentRispository
+    @DeleteMapping(path = "{studentID}")
+    public void deleteStudent(
+            @PathVariable("studentID") Long studentId)
+    {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentID}")
+    public void updateStudent(
+            @RequestParam Long studentID,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email)
+    {
+        studentService.updateStudent(studentID,name,email);
+    }
+
+
+
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,8 +3,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 @Service
 public class StudentService {
     private final StudentRespository studentRespository;
@@ -20,6 +23,40 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional <Student> studentOptional=studentRespository.findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent())
+        {
+            throw new IllegalStateException("email is taken");
+        }
+        studentRespository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        Boolean exits=studentRespository.existsById(studentId);
+        if (!exits)
+            throw new IllegalStateException("student id = "+studentId+" does not exist");
+        studentRespository.deleteById(studentId);
+    }
+    //using this annotation means that we won't use any of the Jpa queries 
+    @Transactional
+    public void updateStudent(Long studentID, String name, String email) {
+        Optional <Student> student=studentRespository.findById(studentID);
+        Optional <Student> studentOptional=studentRespository.findStudentByEmail(email);
+
+        if (student == null )
+            throw new IllegalStateException("student id = "+studentID+" does not exist");
+
+        //student is type optional student so to get access to student methods we need to use the optional method : optionalStudent.get()
+        if (name!=null && name.length()>0 && !Objects.equals(name, student.get().getName()))
+            student.get().setName(name);
+
+
+        if (email!=null && email.length()>0 && !Objects.equals(email, student.get().getEmail()) ) {
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email is taken");
+            }
+            student.get().setEmail(email);
+        }
     }
 }
